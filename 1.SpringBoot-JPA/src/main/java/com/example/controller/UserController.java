cb945622f5d5ee8098c744b53c02607e76c53325
KentTain <tianchangjun@outlook.com>
2019-04-04 08:40:32 +0800

    Create JPA Demo

---
+++ 1.SpringBoot-JPA/src/main/java/com/example/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.example.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class UserController {
+	private static Logger log = LoggerFactory.getLogger(UserController.class);
+}
