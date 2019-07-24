989bd1e1cfdfe39de039d19af079272b4e8b6128
KentTain <tianchangjun@outlook.com>
2019-07-24 16:26:30 +0800

    fixed warning

--- 1.SpringBoot-JPA/src/main/java/com/example/model/MenuNode.java
+++ 1.SpringBoot-JPA/src/main/java/com/example/model/MenuNode.java
@@ -3,13 +3,19 @@ package com.example.model;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name="tb_menu")
 public class MenuNode extends TreeNode<MenuNode> {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6362616183694462936L;
 	private String Desc;
 
 }
